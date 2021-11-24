a95aadc3f4144be6bf919ea7f7a514309d182db7
sjpelayo1 <92770698+sjpelayo1@users.noreply.github.com>
2021-11-23 22:55:17 -0800

    Add files via upload

---
+++ AirlineProject/src/Person.java
@@ -0,0 +1,50 @@
+package myFirstGUI;
+
+public class Person {
+	private String FName;
+	private String LName;
+	private String phoneNumber;
+	private String email;
+	
+	public Person(String FName, String LName, String phoneNumber, String email)
+	{
+		this.FName = FName;
+		this.LName = LName;
+		this.phoneNumber = phoneNumber;
+		this.email = email;
+		
+	}
+	
+	public String getFName()
+	{
+		return FName;
+	}
+	public String getLName()
+	{
+		return LName;
+	}
+	public String getPhoneNumber()
+	{
+		return phoneNumber;
+	}
+	public String getEmail()
+	{
+		return email;
+	}
+	public void setFName(String FName) 
+	{
+		this.FName = FName;
+	}
+	public void setLName(String LName)
+	{
+		this.LName = LName;
+	}
+	public void setEmail(String email)
+	{
+		this.email = email;
+	}
+	public void setphoneNumber(String phoneNumber)
+	{
+		this.phoneNumber = phoneNumber;
+	}
+}
---
+++ AirlineProject/src/reservation.java
@@ -0,0 +1,48 @@
+package myFirstGUI;
+
+public class reservation {
+	private int flightNumber;
+	private String flightTime;
+	private String departingCity;
+	private String destinationCity;
+	
+	public reservation(int flightNumber, String flightTime, String departingCity, String destinationCity)
+	{
+		this.flightNumber = flightNumber;
+		this.flightTime = flightTime;
+		this.departingCity = departingCity;
+		this.destinationCity = destinationCity;
+	}
+	public int getflightNumber() 
+	{
+		return flightNumber;
+	}
+	public String getFlightTime()
+	{
+		return flightTime;
+	}
+	public String getDepartingCity()
+	{
+		return departingCity;
+	}
+	public String getdestinationCity()
+	{
+		return destinationCity;
+	}
+	public void setFlightNumber(int flightNumber)
+	{
+		this.flightNumber = flightNumber;
+	}
+	public void setFlightTime(String flightTime) 
+	{
+		this.flightTime = flightTime;
+	}
+	public void setdepartingCity(String departingCity)
+	{
+		this.departingCity = departingCity;
+	}
+	public void setDestinationCity(String destinationCity)
+	{
+		this.destinationCity = destinationCity;
+	}
+}
