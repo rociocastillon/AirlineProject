567b760842be989e4d4d855ab91dcfac9bf829cc
Rocio <rocio@192.168.1.3>
2021-11-23 23:03:08 -0800

    updated res class

--- AirlineProject/src/reservation.java
+++ AirlineProject/src/reservation.java
@@ -1,16 +1,18 @@
 
 public class reservation {
 	private int flightNumber;
+	private int reservationNumber;
 	private String flightTime;
 	private String departingCity;
 	private String destinationCity;
 	
-	public reservation(int flightNumber, String flightTime, String departingCity, String destinationCity)
+	public reservation(int flightNumber, String flightTime, String departingCity, String destinationCity, int reservationNumber)
 	{
 		this.flightNumber = flightNumber;
 		this.flightTime = flightTime;
 		this.departingCity = departingCity;
 		this.destinationCity = destinationCity;
+		this.reservationNumber = reservationNumber;
 	}
 	public int getflightNumber() 
 	{
@@ -28,6 +30,10 @@ public class reservation {
 	{
 		return destinationCity;
 	}
+	public int getReservationNumber()
+	{
+		return reservationNumber;
+	}
 	public void setFlightNumber(int flightNumber)
 	{
 		this.flightNumber = flightNumber;
@@ -43,5 +49,9 @@ public class reservation {
 	public void setDestinationCity(String destinationCity)
 	{
 		this.destinationCity = destinationCity;
+	}
+	public void setReservationNumber(int reservationNumber)
+	{
+		this.reservationNumber = reservationNumber;
 	}
 }
